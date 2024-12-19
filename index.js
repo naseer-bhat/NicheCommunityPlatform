a8b4dd779571d2c472fb72cf7c09536c19a41ec7
naseer-bhat <studymail2k18@gmail.com>
2024-12-19 11:42:54 +0530

    updated route

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT= process.env.PORT
 connect(database_url).then(() => console.log(`db connected`))
 app.use(express.json())
 
-app.use('/user',userRouter)
+app.use('/api',userRouter)
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
 })
