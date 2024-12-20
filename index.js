e36071151825d4ab3b6c5f5bb8e36a93c874ecd8
naseer-bhat <studymail2k18@gmail.com>
2024-12-20 22:28:49 +0530

    added cors middleware

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import dotenv from 'dotenv'
 import { connect } from "mongoose";
 import userRouter from './routes/userRoute.js';
@@ -8,7 +9,7 @@ const database_url = process.env.db_url
 const PORT= process.env.PORT
 connect(database_url).then(() => console.log(`db connected`))
 app.use(express.json())
-
+app.use(cors())
 app.use('/api',userRouter)
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
