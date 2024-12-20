8f8f770d7b625acb76d61de38b869cd68135baa5
naseer-bhat <studymail2k18@gmail.com>
2024-12-20 21:18:55 +0530

    added

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,7 +3,7 @@ import { userLogin } from "../controllers/loginController.js";
 import { signUp} from '../controllers/signupController.js'
 import {addUser, getUserById} from '../controllers/userController.js'
 const userRouter = Router()
-userRouter.get('/getuser:id', getUserById)
+userRouter.get('/getuser/:id', getUserById)
 userRouter.post('/addUser',addUser)
 userRouter.post('/signup', signUp)
 userRouter.post('/login',userLogin)
