a1a0aaa035c1473489dc43957e3d0b82ea95ca64
naseer-bhat <studymail2k18@gmail.com>
2024-12-20 23:12:42 +0530

    added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const database_url = process.env.db_url
 const PORT= process.env.PORT
 connect(database_url).then(() => console.log(`db connected`))
 app.use(express.json())
-app.use(cors())
+app.use(cors())  
 app.use('/api',userRouter)
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
