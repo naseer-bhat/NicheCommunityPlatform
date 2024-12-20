7b3b7f5c50d0c1dbe3bb51af6befbce2cbcb1d9f
naseer-bhat <studymail2k18@gmail.com>
2024-12-20 21:43:09 +0530

    fixed user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,23 @@
 import {User} from "../models/userMode.js"
-
 export const getUserById = async (req, res) => {
-  const userId = parseInt(req.params.id); 
-  const user = User.find(u => u.id === userId);
+  try {
+    const userId = req.query.id; 
+    if (!userId) {
+      return res.status(400).json({ message: "User ID is required" });
+    }
 
-  if (user) {
+    const user = await User.findById(userId); 
+
+    if (user) {
       res.json(user);
-  } else {
+    } else {
       res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: "Invalid User ID or Server Error" });
   }
-}
+};
+
 export const addUser= async (req, res) => {
   try {
       const user = new User(req.body);
