89d482b31fc7e18780722529a77dede148c5ff5b
naseer-bhat <studymail2k18@gmail.com>
2024-12-21 13:52:36 +0530

    added auth to comment ap

--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { addComment, getCommentsByPost } from "../controllers/commentController.js";
+import { userLogin } from "../controllers/loginController.js";
 
 const router = express.Router();
-router.post("/add", addComment);
-router.get("/:postId", getCommentsByPost);
+router.post("/add",userLogin, addComment);
+router.get("/:postId",userLogin,getCommentsByPost);
 export default router;
